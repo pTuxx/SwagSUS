1f92126ad1c8a0ac76e6e8b7353d8ac47e3bbae1
pTux <55540562+pTuxx@users.noreply.github.com>
2021-10-28 19:13:02 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -36,18 +36,21 @@ local function getDeviceType()
  end
  
 local function Install()
-  if not fs.exists("versions/os_version.txt") or tonumber(read_file("versions/os_version.txt")) < tonumber(gitgrab("pTuxx", "SwagSUS", "main/versions", "os_version.txt").readAll()) then
+  if not fs.exists("versions/os_version.txt") or tonumber(read_file("versions/os_version.txt")) < tonumber(gitgrab("pTuxx", "SwagSUS", "main/versions", "os_version.txt")) then
     github("pTuxx", "SwagSUS", "main/versions", "os_version.txt", "versions/os_version.txt")
     github("pTuxx", "SwagSUS", "main", "startup.lua", "startup.lua")
     shell.run("reboot")
   end
 
-  if not fs.exists("versions/doorlock_version.txt") or tonumber(read_file("versions/doorlock_version.txt")) < tonumber(gitgrab("pTuxx", "SwagSUS", "main/versions", "doorlock_version.txt").readAll()) then
+  if not fs.exists("versions/doorlock_version.txt") or tonumber(read_file("versions/doorlock_version.txt")) < tonumber(gitgrab("pTuxx", "SwagSUS", "main/versions", "doorlock_version.txt")) then
     github("pTuxx", "SwagSUS", "main/versions", "doorlock_version.txt", "versions/doorlock_version.txt")
     github("pTuxx", "SwagSUS", "main/programs", "doorlock.lua", "programs/doorlock.lua")
   end
 
-  
+  if not fs.exists("versions/rcturtle_version.txt") or tonumber(read_file("versions/rcturtle_version.txt")) < tonumber(gitgrab("pTuxx", "SwagSUS", "main/versions", "rcturtle_version.txt")) then
+    github("pTuxx", "SwagSUS", "main/versions", "rcturtle_version.txt", "versions/rcturtle_version.txt")
+    github("pTuxx", "SwagSUS", "main/programs", "rcturtle.lua", "programs/rcturtle.lua")
+  end
 end
  
  local function startup1()
